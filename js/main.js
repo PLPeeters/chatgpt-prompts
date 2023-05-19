5b630b2ff6b19ac9aa9b8445dccf12119bd0c70c
Pierre-Louis Peeters <pierrelouis.peeters@gmail.com>
2023-05-19 14:59:58 +0200

    Make it possible to specify that a certain placeholder should be a textarea

--- js/main.js
+++ js/main.js
@@ -27,44 +27,71 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Process each prompt
     prompts.forEach(prompt => {
         // Extract placeholders from the prompt text
-        var re = /\[(.*?)\]/g;
+        var re = /\[(.*?)(?::(.*?))?\]/g;
         var text = prompt.dataset.originalText;
         var match;
         var placeholders = [];
 
         while ((match = re.exec(text)) != null) {
-            if (placeholders.indexOf(match[1]) === -1) {
-                placeholders.push(match[1]);
+            if (placeholders.map(p => p.name).indexOf(match[1]) === -1) {
+                if (match[2] === 'textarea') {
+                    placeholders.push({
+                        'name': match[1],
+                        'elementType': 'textarea'
+                    })
+                } else {
+                    placeholders.push({
+                        'name': match[1],
+                        'elementType': 'input'
+                    });
+                }
             }
         }
 
         var inputs = document.createElement('div');
         inputs.setAttribute('class', 'inputs');
 
+        var updatedText = text;
+
         // For each placeholder, create an input element
         placeholders.forEach(placeholder => {
             // Create a new input element
-            var input = document.createElement('input');
-            input.setAttribute('type', 'text');
+            var input = document.createElement(placeholder.elementType);
+
+            if (placeholder.elementType === 'input') {
+                input.setAttribute('type', 'text');
+            } else if (placeholder.elementType === 'textarea') {
+                input.addEventListener('input', function() {
+                    this.style.height = 'auto';
+                    this.style.height = this.scrollHeight + 'px';
+                }, false);
+                input.style.resize = 'none';
+            }
+
             input.setAttribute('class', `placeholder-input ${prompt.id}-placeholder-input`);
-            input.setAttribute('name', placeholder);
-            input.setAttribute('data-placeholder', placeholder);
+            input.setAttribute('name', placeholder.name);
+            input.setAttribute('placeholder', placeholder.name);
+            input.setAttribute('data-placeholder', placeholder.name);
             input.setAttribute('data-prompt', prompt.id);
 
             var label = document.createElement('label');
-            label.setAttribute('for', placeholder);
-            label.textContent = `${placeholder}:`;
+            label.setAttribute('for', placeholder.name);
+            label.setAttribute('for-type', placeholder.elementType);
+            label.textContent = `${placeholder.name}:`;
 
             inputs.appendChild(label);
             inputs.appendChild(input);
+
+            updatedText = updatedText.replace(new RegExp(`\\[${placeholder.name}(:.*?)?\\]`, 'g'), `[${placeholder.name}]`);
         });
 
         // Insert the input element before the prompt
         prompt.parentNode.insertBefore(inputs, prompt);
+        prompt.textContent = updatedText;
     });
 
     // Grab all input elements
-    var inputs = document.querySelectorAll('input[data-placeholder]');
+    var inputs = document.querySelectorAll('[data-placeholder]');
 
     // Add event listener to each input element
     inputs.forEach(input => {
@@ -79,7 +106,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
             // Replace placeholders in text with input values
             inputs.forEach(input => {
-                updatedText = updatedText.replace(new RegExp(`\\[${input.name}\\]`, 'g'), input.value || `[${input.name}]`)
+                updatedText = updatedText.replace(new RegExp(`\\[${input.name}(:.*?)?\\]`, 'g'), input.value || `[${input.name}]`)
             })
 
             // Update the text of the prompt
